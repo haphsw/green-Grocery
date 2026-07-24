828a1ed59461d50c603f6a8ecb42e7a6180baad1
haphsw <hafswhussein@gmail.com>
2026-07-24 23:57:48 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+function displayMessage() {
+
+    alert("Welcome to Green Grocery!\n\nWe provide fresh fruits, vegetables, and quality grocery products at affordable prices.\n\nThank you for choosing Green Grocery!");
+
+}
